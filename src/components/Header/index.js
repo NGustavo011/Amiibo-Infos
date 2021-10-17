9b864ba72f81fcb588d45bc49fe8c5ba984d9311
NGustavo011 <gsn181200@gmail.com>
2021-10-17 01:26:25 -0300

    working home button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as S from './styled';
 import logo from '../../assets/images/AmiiboLogo.png';
 import useListCard from '../../hooks/ListCardHooks';
@@ -6,15 +6,21 @@ import useListCard from '../../hooks/ListCardHooks';
 const Header = () =>{
     const {listNameState, getAmiibo, goHome} = useListCard();
     const [ optionValue, setOptionValue ] = useState();
-
-    useEffect(()=>{
-        console.log(optionValue);
-    }, [optionValue]);
+    const selectInputRef = useRef();
 
     const onSubmit = (e) =>{
         e.preventDefault();
         getAmiibo(optionValue);
     }
+
+    const backToHome = () =>{
+        selectInputRef.current.setValue({label: null, value: null});
+        goHome();
+    }
+    
+    useEffect(()=>{
+        console.log(optionValue);
+    }, [optionValue]);
 
   return (
       <S.Wrapper>
@@ -22,7 +28,7 @@ const Header = () =>{
                 <S.Logo src={logo} />
             </S.WrapperLogo>
             <S.FormAmiibo>
-                <S.HomeOption>
+                <S.HomeOption onClick={backToHome}>
                     <S.HomeText className="fas fa-home" />
                 </S.HomeOption>
                 <S.SelectAmiibo
@@ -33,6 +39,7 @@ const Header = () =>{
                     options={
                         listNameState.sort((a,b) => (a.label > b.label) ? 1 : ((b.label > a.label) ? -1 : 0)).filter((v,i,a)=>a.findIndex(t=>(t.label === v.label))===i)
                     }
+                    ref={selectInputRef}
                     onChange={(event) => setOptionValue(event.value)}
                 />
                 <S.ButtonAmiibo type="submit" onClick={(e)=>onSubmit(e)}>
